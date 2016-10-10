89285b628f8eea027ed829ab545d84313f034960
Henrich Horváth <dev@henrich-horvath.eu>
2016-10-11 00:09:36 +0200

    initial commit

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Hlavny manazer hry (singleton)
+/// </summary>
+public class GameManager : MonoBehaviour
+{
+    [SerializeField]
+    private Dyno dyno;
+
+    [SerializeField]
+    private TrackManager trackManager;
+
+    private static GameManager _instance;
+
+    /// <summary>
+    /// Vrati instanciu na game manager singleton
+    /// </summary>
+    public static GameManager instance
+    {
+        get
+        {
+            if (_instance == null)
+            {
+                _instance = (GameManager) FindObjectOfType(typeof(GameManager));
+            }
+
+            return _instance;
+        }
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
---
+++ Assets/Scripts/TrackManager.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Generovanie trate
+/// </summary>
+public class TrackManager : MonoBehaviour
+{
+	// Use this for initialization
+	void Start ()
+    {	
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {	
+	}
+}
