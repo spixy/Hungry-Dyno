dc228470f7aa5b116572d5230dff69321b9a7f08
Henrich Horváth <dev@henrich-horvath.eu>
2016-10-28 00:59:39 +0200

    added debug UI, scripts moved to subfolders

---
+++ Assets/Scripts/Other/DebugUI.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+public class DebugUI : MonoBehaviour
+{
+#if UNITY_EDITOR || DEBUG
+    void Awake()
+    {
+        this.gameObject.SetActive(false);
+    }
+#endif
+}
--- Assets/Scripts/Player/Dyno.cs
+++ Assets/Scripts/Player/Dyno.cs
@@ -8,13 +8,31 @@ public class Dyno : MonoBehaviour
     [SerializeField]
     private Animator anim;
 
+    private Bonus activeBonus;
+
     void Start()
     {
+        this.activeBonus = Bonus.None;
     }
-    
+
     void Update()
     {
         // 1 meter = 1 bod ?
         GameManager.Instance.Score = (int) this.transform.position.x;
     }
+
+    private void OnBonusStarted(Bonus bonus)
+    {
+        this.activeBonus = bonus;
+
+        // bla bla
+    }
+
+    private void OnBonusEnded()
+    {
+        this.activeBonus = Bonus.None;
+
+        // bla bla
+    }
+
 }
