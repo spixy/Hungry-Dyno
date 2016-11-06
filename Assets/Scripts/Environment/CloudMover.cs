d271865f2f6ab33f009ef85e207e0f167ce8ee78
Henrich Horváth <dev@henrich-horvath.eu>
2016-11-06 23:20:12 +0100

    imported assets

---
+++ Assets/Scripts/Environment/CloudMover.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class CloudMover : MonoBehaviour
+{
+    private float speed;
+
+	void OnEnable()
+    {
+        this.speed = Random.Range(0.04f, 0.14f);
+    }
+	
+	void Update ()
+    {
+	    transform.Translate(this.speed * Time.deltaTime, 0f, 0f);
+    }
+}
