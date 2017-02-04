74fabe54f31b20b0b7ce25cf4dca7c1f6bedd229
oreqizer <oreqizer@gmail.com>
2017-02-04 18:06:57 +0100

    balance changes

--- Assets/Scripts/Environment/AutoSpawner.cs
+++ Assets/Scripts/Environment/AutoSpawner.cs
@@ -9,6 +9,8 @@ public class AutoSpawner : Spawner
     [SerializeField]
     private float timerDelay = 0f;
 
+    private float factor = 1f;
+
     private Coroutine coroutine = null;
 
     public void Start()
@@ -16,11 +18,20 @@ public class AutoSpawner : Spawner
         this.coroutine = StartCoroutine(this.SpawningLoop());
     }
 
+    public void Update() {
+        float f = 1f - (transform.position.x - 1000) / 1000;
+        if (f > 1f) {  // speed it up to 300%
+            factor = 1f;
+        } else if (f > 0.33f) {
+            factor = f;
+        }
+    }
+
     private IEnumerator SpawningLoop()
     {
         while (true)
         {
-            yield return new WaitForSeconds(timer + Random.value * timerDelay); ;
+            yield return new WaitForSeconds(timer + Random.value * (timerDelay * factor));
 
             if (GameManager.Instance.State == State.InGame)
 				this.Spawn(this.transform.position.x);
