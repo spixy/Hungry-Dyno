82dd597993fd9716f4079914fce89b11c83df427
oreqizer <oreqizer@gmail.com>
2017-02-09 14:55:22 +0100

    fix parallax

---
+++ Assets/BgRunner.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class BgRunner : MonoBehaviour {
+    [SerializeField]
+    private Transform player;
+
+    [SerializeField]
+    private float speedRatio;
+
+    [SerializeField]
+    private bool stopOnReverse;
+    
+    private float lastX;
+
+    void Start() {
+        lastX = player.position.x;
+    }
+
+    void LateUpdate() {
+        if (stopOnReverse && player.position.x <= lastX)
+            return;
+
+        this.transform.position = new Vector3(this.player.position.x * speedRatio, this.transform.position.y, -1f);
+        lastX = this.player.position.x;
+    }
+}
